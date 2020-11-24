62bf016fdf66f45a5657c00ef3ae7fbde08fe4b5
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-11-24 16:33:03 +0100

    Install styled-components and create GlobalStyle.js

---
+++ frontend/src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+
+:root {
+
+}
+
+*{
+    boz-sizing:border-box;
+}
+
+body {
+    padding:0;
+    margin:0;
+    font-size: 112.5%;
+}
+
+
+`
