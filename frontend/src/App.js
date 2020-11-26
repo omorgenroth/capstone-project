bbd23bd9cb27ae3f58b1a5cfb32828dc8fe62cda
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-11-26 14:07:51 +0100

    Refacoring after Review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
   )
 
   function addIsCheckedValue(data) {
-    return data.map((o) => ({ ...o, isChecked: true }))
+    return data.map((item) => ({ ...item, isChecked: true }))
   }
 }
 
