743750a03f8e0c588a9ad5f7f13ad17520f15bde
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-12-01 15:13:53 +0100

    Fix Tests for DishItem/ListItem

--- frontend/src/Components/DishItem/DishItem.test.js
+++ frontend/src/Components/DishItem/DishItem.test.js
@@ -4,21 +4,21 @@ import DishItem from './DishItem'
 describe('DishItem', () => {
   it('renders correctly', () => {
     const { container } = render(
-      <DishItem isSelected={false} title="My Dish" />
+      <DishItem isSelected={false} id={1} title="My Dish" />
     )
     expect(container.firstChild).toMatchSnapshot()
   })
 
   it('renders with the CheckedIcon if isSelected is true', () => {
     const { getByTestId } = render(
-      <DishItem isSelected={true} title="My Dish" />
+      <DishItem isSelected={true} id={1} title="My Dish" />
     )
     expect(getByTestId('checked')).toBeInTheDocument()
   })
 
   it('renders with the UnCheckedIcon if isSelected is false', () => {
     const { getByTestId } = render(
-      <DishItem isSelected={false} title="My Dish" />
+      <DishItem isSelected={false} id={1} title="My Dish" />
     )
     expect(getByTestId('unchecked')).toBeInTheDocument()
   })
