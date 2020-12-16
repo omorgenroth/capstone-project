0a7b39aaaf7e782bfba471d81ced95c6e1b96c28
Oliver Morgenroth <morgenroth.oliver@gmail.com>
2020-12-16 17:58:16 +0100

    Add fetch Services for Ingredients and Active Lists

--- frontend/src/services/fetchUsers.js
+++ frontend/src/services/fetchUsers.js
@@ -1,8 +1,18 @@
 const baseUrl = 'http://capstone.local/users'
 
-export async function getUserLists(id) {
+export async function getUserLists(userId) {
   try {
-    const response = await fetch(baseUrl + '/' + id + '/lists')
+    const response = await fetch(baseUrl + '/' + userId + '/lists')
+    const data = response.json()
+    return data
+  } catch (err) {
+    return { error: true }
+  }
+}
+
+export async function getActiveUserList(userId) {
+  try {
+    const response = await fetch(baseUrl + '/' + userId + '/lists/active')
     const data = response.json()
     return data
   } catch (err) {
